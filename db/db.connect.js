a1cbb82dd2acf08a1e50365080ce74c111b812b0
Sam Sweeney <samsweeneyy@gmail.com>
2023-02-16 19:42:40 +1100

    Changed db connection

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
-
+require("dotenv").config();
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "password",
 });
 
 module.exports = {connection}
